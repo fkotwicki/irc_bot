bf63cfdff722ba7f61594f3998e2701b98db45b8
fkotwicki <filip.kotwicki@gmail.com>
2014-01-07 18:39:25 +0100

    Fix

--- src/main/scala/com/vkr/ircbot/common/commands/CtcpCommand.scala
+++ src/main/scala/com/vkr/ircbot/common/commands/CtcpCommand.scala
@@ -1,7 +1,7 @@
 package com.vkr.ircbot.common.commands
 import com.vkr.ircbot.io.SimpleCommand
 
-class PongCommand(prefix: String) extends SimpleCommand(prefix) {
+class CtcpCommand(prefix: String) extends SimpleCommand(prefix) {
   
   override def getValue(command: String) : Array[String] = {
     Array("VKR BOT VER 1.0")
